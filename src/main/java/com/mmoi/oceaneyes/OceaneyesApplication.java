6271baeca662460ef76d9c9efcb780ec54b116fe
SASHA <mmmoikim@gmail.com>
2020-04-19 16:30:00 +0900

    Initial commit

---
+++ src/main/java/com/mmoi/oceaneyes/OceaneyesApplication.java
@@ -0,0 +1,13 @@
+package com.mmoi.oceaneyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceaneyesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OceaneyesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mmoi/oceaneyes/OceaneyesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmoi.oceaneyes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceaneyesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
